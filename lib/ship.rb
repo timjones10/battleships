241eff4b38ed3b6dd3d08de4b962bd0f010fa783
Tim Jones <timjones10@hotmail.com>
2017-11-26 12:08:58 +0000

    Hit method added to ship object so that it can record a hit

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,11 +1,16 @@
 class Ship
 
-attr_reader :type, :size
+attr_reader :type, :size, :hit_count
 
   def initialize(type)
     @type = type
     @size
+    @hit_count = 0
     ship_size(type)
+  end
+
+  def hit
+    @hit_count += 1
   end
 
   private
--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -1,15 +1,19 @@
 require 'ship'
 
 describe 'ship' do
+  ship = Ship.new(:carrier)
 
   it 'is initiated with a type' do
-    ship = Ship.new(:carrier)
     expect(ship.type).to eq :carrier
   end
 
   it 'knows its size which is determined by its type' do
-  ship = Ship.new(:carrier)
   expect(ship.size).to eq 5
+  end
+
+  it 'can recieve a hit' do
+    ship.hit
+    expect(ship.hit_count).to eq 1
   end
 
 end
